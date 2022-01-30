93496d5f72d2a9d1bc5c0b0144e92e26e7197d42
Natanael A. de Morais <natanaelallves@gmail.com>
2022-01-30 18:53:25 -0300

    slá

--- app_placas/migrations/0003_alter_placa_imagem.py
+++ app_placas/migrations/0003_alter_placa_imagem.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='placa',
             name='imagem',
-            field=models.CharField(blank=True, max_length=50, null=True),
+            field=models.CharField(blank=True, max_length=255, null=True),
         ),
     ]
