646609da89c6fd18b36cfc9564769df5eaadad8f
Natanael A. de Morais <natanaelallves@gmail.com>
2022-01-30 23:21:06 -0300

    order by placas

--- app_placas/views.py
+++ app_placas/views.py
@@ -6,6 +6,6 @@ from .models import Placa
 
 def index(request):
     data = dict()
-    data['regulamentacao'] = Placa.objects.filter(categoria='reg')
-    data['advertencia'] = Placa.objects.filter(categoria='adv')
+    data['regulamentacao'] = Placa.objects.filter(categoria='reg').order_by('pk')
+    data['advertencia'] = Placa.objects.filter(categoria='adv').order_by('pk')
     return render(request, 'app_placas/index.html', data)
